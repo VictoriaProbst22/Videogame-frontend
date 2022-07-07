746418c0d64cb371c2da8f1efdd231ebec8504e4
Victoria Probst <victoria.probst13@gmail.com>
2022-07-07 15:10:37 -0400

    Created Async Function to Get Game Data and print to Console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
 
 function App() {
+
+useEffect(()=>{
+  getGameData();
+}, [])
+
+
+const getGameData = async() => {
+  try {
+    let response = await axios.get("http://localhost:8080/all");
+    console.log(response.data)
+    
+  } catch (error) {
+    console.log(error)
+    
+  }
+}
+
   return (
     <div>
       <h3> Hello World!</h3>
